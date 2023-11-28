7640920f34dd1a796065a39ff7428d605b298bc7
HucksApp <hucksapp@gmail.com>
2023-11-28 12:11:39 -0800

    q8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def upperchar(c: str) -> int:
+    asc = (ord(c) - 32) if (ord(c) >= 97 and ord(c) <= 122) else ord(c)
+    return asc
+
+
+def uppercase(str: str) -> None:
+    ret = ""
+    for chars in str:
+        ret += "%c" % uppercase(chars)
+    print("{:s}".format(ret))
+
