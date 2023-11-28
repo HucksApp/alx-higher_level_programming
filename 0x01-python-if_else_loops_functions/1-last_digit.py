09f7682b1c0ed312a4ec470d20417875538a0e2a
HucksApp <hucksapp@gmail.com>
2023-11-28 09:15:58 -0800

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# access the str rep of num and pick last
-lst = int(repr(number)[-1])
 gr = "greater than 5"
 lss = "less than 6 and not 0"
+lst = number % 10 if number >= 0 else (-number % 10) * -1
 fmt = gr if lst > 5 else lss if lst < 6 and lst != 0 else "0"
 print(f"Last digit of {number} is {lst} and is {fmt}")
-
