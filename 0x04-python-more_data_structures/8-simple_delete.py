32dc161860a99446c0e979c5b72f20a867252aba
HucksApp <hucksapp@gmail.com>
2023-12-06 13:51:53 -0800

    q8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary: dict, key=""):
-    a_dictionary.pop(key)
+def simple_delete(a_dictionary: dict, key: str = "") -> dict:
+    if key:
+        a_dictionary.pop(key)
     return a_dictionary
