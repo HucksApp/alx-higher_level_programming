dc830f481c803c29fcd941544ca1f155ce2bb668
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-04 14:07:35 -0800

    Update 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
         """Return a string representation of a Rectangle instance
         that is able to recreate a new instance by using eval()
         """
-        return (f"{self.__class__.__name__}({self.__width}, {self.__height})")
+        return "{}({}, {})".format(self.__class__.__name__, self.__width, self.__height)
 
     @property
     def width(self) -> int:
