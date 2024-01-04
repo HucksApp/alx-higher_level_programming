26c267f409bfed9f684e2eed28a55c31903d1714
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-04 15:26:47 -0800

    q8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -105,7 +105,7 @@ class Rectangle:
         for x in (rect_1, rect_2):
             if not isinstance(x, Rectangle):
                 raise TypeError(f"{'rect_1' if x == rect_1 else 'rect_2'} 
-                must be an instance of Rectangle")
+                    must be an instance of Rectangle")
         if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
             return rect_1
         else:
