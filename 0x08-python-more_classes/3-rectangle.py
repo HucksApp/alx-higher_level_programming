a68b082e8743e5b00735fbbe243b330117670177
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-04 12:14:43 -0800

    q3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -20,6 +20,8 @@ class Rectangle:
     def __str__(self):
         """Returns an informal and nicely printable string representation
         of a Rectangle instance, filled with the '#' character."""
+        if self.__height == 0 or self.__width == 0:
+            return ''
         rep = ""
         for h in range(self.__height):
             for w in  range(self.__width):
