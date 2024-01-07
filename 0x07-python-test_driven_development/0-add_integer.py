6d55aecc86b7359b13877b0da247d346e052011f
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-07 09:44:33 -0800

    q0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,17 +6,15 @@
 
 
 
-def add_integer(a: int| float, b=98)-> int:
+def add_integer(a: int | float, b: int | float = 98) -> int:
     '''Returns an integer: the addition of a and b
         where a and b is of type float or int
     '''
     add_sum = 0
-    for x in (a,b):
+    for x in (a, b):
         if type(x) is not int and type(x) is not float:
             raise TypeError(f'{"a" if x == a else "b"} must be an integer')
         if type(x) is float:
             x = int(x)
         add_sum += x
     return (add_sum)
-
-
