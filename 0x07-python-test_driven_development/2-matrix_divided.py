cfa377edd5b5caaf4139865c762a5cf850facfa8
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-07 10:20:16 -0800

    q2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -24,7 +24,7 @@ def matrix_divided(matrix: list, div: int | float) -> list:
         for elem in shape:
             if type(elem) is not int and type(elem) is not float:
                 msg = "matrix must be a matrix (list of lists) "
-                raise TypeError((msg + "of integers/floats")
+                raise TypeError(msg + "of integers/floats")
             shape_matrix.append(round(elem/div, 2))
         div_matrix.append(shape_matrix)
     return div_matrix
