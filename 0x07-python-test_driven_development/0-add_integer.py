b2cfd829d3c97452b8ecab150ad77726cd09ccb3
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-08 01:35:55 -0800

    q0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,12 +4,10 @@ Module add integer
 Adds two integers
 """
 
-
 def add_integer(a: int | float, b: int | float = 98) -> int:
     """Returns an integer: the addition of a and b
         where a and b is of type float or int
     """
-
 
     add_sum = 0
     for x in (a, b):
