29d61f3bd48c046d86089c86aff344cd8ca846b6
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-08 06:55:12 -0800

    Update 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,7 +5,7 @@ Matrix multiplication using NumPy.
 import numpy as np
 
 
-def lazy_matrix_mul(m_a = [], m_b = []):
+def lazy_matrix_mul(m_a, m_b):
     """Multiplies m_a and m_b using
     matmul, and returns the result.
     """
