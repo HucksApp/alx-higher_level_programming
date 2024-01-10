4336dd2aaa03bfb5a5f90cc5a24ecf7bfa0825f1
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-09 19:04:33 -0800

    :recycle: 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,11 +10,9 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The string to search for within the file.
         new_string (str): The string to insert.
     """
-    text = ""
     with open(filename) as r:
         for line in r:
-            text += line
             if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+                set_point = r.tell()
+                r.seek(set_point)
+                r.write(new_string)
