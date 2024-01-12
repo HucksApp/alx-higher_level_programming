19edfb79dc429b287427e3d6b04c4c2621c01353
HucksApp <hucksapp@gmail.com>
2024-01-12 11:01:43 -0800

    :bug: 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,56 +5,53 @@ prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
-from sys import stdin, exit
 
 
-def stats_metrics():
-    """Reads from standard input, computes metrics
-        and print metrics
-    """
-    total_size = 0
-    stats = {'200': 0, '301': 0, '400': 0, '401': 0,
-             '403': 0, '404': 0, '405': 0, '500': 0}
-
-    try:
-        line_no = 0
-        for line in stdin:
-            total_size += parse_metrics(line, stats)
-            line_no += 1
-            if line_no == 10:
-                line_no = 0
-                print_stats(total_size, stats)
-
-    except KeyboardInterrupt:
-        print_stats(total_size, stats)
-        raise
-
-
-def print_stats(total_size, stats):
+def print_stats(size, status_codes):
     """Print accumulated metrics.
     Args:
-        total_size: The accumulated read file size.
-        stats: The accumulated count of status codes.
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
-    print(f"File size: {total_size}")
-    for key in sorted(stats):
-        if stats[key]:
-            print(f"{key}: {stats[key]}")
-
-
-def parse_metrics(line, stats):
-    """parse the current line
-    Args:
-        str: current stdin line
-        stats: The accumulated count of status codes.
-    """
-    values = line[line.rfind('"') + 1: -1].strip().split(" ")
-    if len(values) != 2:
-        return 0
-    if values[0] in [*stats]:
-        stats[values[0]] += 1
-    return int(values[1]) if values[1] else 0
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
-    stats_metrics()
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
