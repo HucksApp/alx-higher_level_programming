90355e02f66324583b186038db1929cc5eae0d2a
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-15 18:07:01 -0800

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -136,5 +136,5 @@ class Rectangle(Base):
         '''
             Overwritting the str method
         '''
-        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__, self.id,
-                                                self.x, self.y, self.width, self.height)
+        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__, self.id, self.x,
+                                                self.y, self.width, self.height)
