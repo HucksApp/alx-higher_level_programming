8e12072c2906beb778a878a80e260e5c86150bb8
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-17 14:26:07 -0800

    :hammer: test

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -35,5 +35,34 @@ class TestBase_to_json_sring(unittest.TestCase):
         self.assertEqual(correct, result)
 
 
+class TestBase_from_json_sring(unittest.TestCase):
+
+    def test_empty_json_string(self):
+        correct = []
+        result = Base.from_json_string("[]")
+        self.assertEqual(correct, result)
+    
+    def test_json_string_isNone(self):
+        correct = []
+        result = Base.from_json_string(None)
+        self.assertEqual(correct, result)
+
+    def test_rect_filled_json_string(self):
+        r = Rectangle(10, 7, 2, 8, 1)
+        str_fmt = str([r.to_dictionary()]).replace("'", '"')
+        result = Base.from_json_string(str_fmt)
+        correct = [{"x": 2, "y": 8, "id": 1, "height": 7, "width": 10}]
+        self.assertEqual(correct, result)
+    
+    def test_square_filled_dictlist(self):
+        s = Square(5)
+        str_fmt = str([s.to_dictionary()]).replace("'", '"')
+        result = Base.from_json_string(str_fmt)
+        correct = [{"id": 2, "x": 0, "size": 5, "y": 0}]
+        self.assertEqual(correct, result)
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
