fdce443befa98a73ed5ec037096ecaea663936c0
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-16 23:52:58 -0800

    :recycle: base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -116,7 +116,7 @@ class Base:
             if not list_objs:
                 file.writerow("[]")
                 return
-            mid = ["size"] if cls is Square else ["width", "height"]
+            mid = ["size"] if cls.__name__ == 'Square' ["width", "height"]
             fieldnames = ["id", *mid, "x", "y"]
 
             csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
