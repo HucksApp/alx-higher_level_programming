585f7738f0ff4a4b070401cf07d63688b835a321
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-17 19:44:48 -0800

    :recycle: test

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -9,7 +9,7 @@ from models.square import Square
 
 
 
-class TestSquare(unittest.TestCase):
+class TestSquare_args(unittest.TestCase):
 
     def test_no_args(self):
         with self.assertRaises(TypeError):
@@ -33,3 +33,50 @@ class TestSquare(unittest.TestCase):
         s1 = Square(1, 2, 3, 4)
         s2 = Square(4, 3, 2, 1)
         self.assertEqual(s1.id, s2.id - 1)
+
+
+class TestSquare_create(unittest.TestCase):
+    """Unittests for testing create method of the Square class."""
+
+    def test_create_obj_type(self):
+        correct = Square(1, 2, 3, 4)
+        result = Square.create(**(correct.to_dictionary()))
+        self.assertIsNot(correct, result)
+        self.assertIsInstance(result, Square)
+
+
+    def test_create_obj_props(self):
+        s1 = Square(4, 3, 2, 1)
+        s2 = Square.create(**(s1.to_dictionary()))
+        correct = s1.to_dictionary()
+        result = s2.to_dictionary()
+        self.assertDictEqual(correct, result)
+        self.assertDictEqual(s1.__dict__, s2.__dict__)
+
+
+    def test_create_args(self):
+        d = {'id': 1, 'size': 2, 'x': 3, 'y': 4 }
+        d2 = [1, 2, 3, 4]
+        r1 = Square(*d2)
+        m = []
+        d3 = {}
+        index = 0
+        for key, value in d.items():
+            if value == d2[index]:
+                d3.update({key: value})
+                m.append(Square.create(**d3))
+            index +=1
+        former : 'Square' = None
+        for obj in m:
+            self.assertIsNot(obj, r1)
+            self.assertIsInstance(obj, Square)
+            result = [*obj.to_dictionary()]
+            result.sort()
+            correct = [*d]
+            correct.sort()
+            self.assertEqual(result, correct)
+            if not former:
+                former = obj
+                continue
+            self.assertIsNot(former, obj)
+        
