ede13e3e978d3d86d5368f94eecbd83ca1503495
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-11 12:08:11 -0700

    :brain:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 
 switch(process.argv.length) {
-  case 2:
-    console.log('No argument');
-    break;
-  case 3:
-    console.log('Argument found');
-    break;
-  default:
-    console.log('Arguments found');
+    case 2: {
+        console.log('No argument');
+        break;
+    }
+    case 3: {
+        console.log('Argument found');
+        break;
+    }
+    default: {
+        console.log('Arguments found');
+    }
 }
+
