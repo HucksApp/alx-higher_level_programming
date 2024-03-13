d88b8644c528e54afcd0fa6df057b462ca57fd7b
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-12 17:14:10 -0700

    :zap:

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       let m = '';
-      for (let x = 0; x < this.width; x++) { m += 'x'; }
+      for (let x = 0; x < this.width; x++) { m += 'X'; }
       console.log(m);
     }
   }
