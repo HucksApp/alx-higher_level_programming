fb080eea7bb1ef052c863846453d54589eb5f9ba
HucksApp <hucksapp@gmail.com>
2024-03-12 19:50:27 -0700

    :zap:

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
+<<<<<<< HEAD
 let valList =  Object.entries(d)
 let out = {}
   while (valList.length) {
@@ -17,6 +18,23 @@ let out = {}
             }
             valList.splice(valList.indexOf(member), 1)
         } 
+=======
+const valList = Object.entries(d);
+const out = {};
+while (valList.length) {
+  const cur_val = valList[0][1];
+  for (const member of valList) {
+    const [key, value] = member;
+    if (value === cur_val) {
+      if (key in out) {
+        out[cur_val].push(key);
+      } else {
+        out[cur_val] = [];
+        out[cur_val].push(key);
+      }
+      valList.splice(valList.indexOf(member), 1);
+>>>>>>> :zap:
     }
   }
-console.log(out)
+}
+console.log(out);
