e78fd7c7e557ee832383eb690084f964baec808a
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-12 17:54:18 -0700

    :zap:

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,9 +6,9 @@ class Square extends SquareP {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.height; i++) {
       let m = '';
-      for (let x = 0; x < this.size; x++) { m += c; }
+      for (let x = 0; x < this.width; x++) { m += c; }
       console.log(m);
     }
   }
