c0fcd33b9fda3bdf92836b1e1727bfda67bf6c26
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-12 18:23:20 -0700

    :zap:

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const Revlist = [];
+  const revList = [];
   for (let x = list.length - 1; x >= 0; x--) {
-    Revlist.push(list[x]);
+    revList.push(list[x]);
   }
-  return list;
+  return revList;
 };
