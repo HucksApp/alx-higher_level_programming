87f443b50b2a2bc49947207704fd14b41e1609eb
HucksApp <hucksapp@gmail.com>
2024-03-12 18:21:32 -0700

    :zap:

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let Revlist = [];
-  for (let x = list.length - 1; x >= 0;  x--) {
-    Revlist.push(list[x])
+  const Revlist = [];
+  for (let x = list.length - 1; x >= 0; x--) {
+    Revlist.push(list[x]);
   }
   return list;
 };
