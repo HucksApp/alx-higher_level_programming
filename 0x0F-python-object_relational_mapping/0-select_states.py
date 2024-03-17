18f120ec0dc0259145a8f03141afd877870f8022
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-16 23:16:58 -0700

    :zap:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,12 +4,12 @@ from MySQLdb import connect
 from sys import argv
 
 
-def connectDb():
+def connectDb(query:str) -> None:
     """ lists all states from the database hbtn_0e_0_usa """
     db = connect(host="localhost", user=argv[1],
                  passwd=argv[2], db=argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
+    cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
@@ -18,4 +18,5 @@ def connectDb():
 
 
 if __name__ == "__main__":
-    connectDb()
+    query = "SELECT * FROM states"
+    connectDb(query)
