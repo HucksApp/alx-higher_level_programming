94f4c9168e73e05cf396e1255a6e51ac03ae76a0
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-16 23:21:22 -0700

    :zap:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Defines a Mysqldb query function"""
 from MySQLdb import connect
 from sys import argv
 
 
 def connectDb(query: str) -> None:
-    """ lists all states from the database hbtn_0e_0_usa """
+    """Query the Mysql database"""
     db = connect(host="localhost", user=argv[1],
                  passwd=argv[2], db=argv[3], port=3306)
     cursor = db.cursor()
