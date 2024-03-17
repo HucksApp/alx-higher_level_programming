ff67379445fe5a99add65eff7d9d09c17d66fe61
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-16 23:15:49 -0700

    :zap:

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+from MySQLdb import connect
+from sys import argv
+
+
+def connectDb(query:str) -> None:
+    """ Query the database hbtn_0e_0_usa """
+    db = connect(host="localhost", user=argv[1],
+                 passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+  query = "SELECT * FROM states WHERE name \
+                LIKE BINARY 'N%' ORDER BY states.id"
+    connectDb(query)
