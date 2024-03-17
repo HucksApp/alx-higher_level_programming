c319b77a257275e3d2c7478e61ba80dffc7f5a87
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-17 12:08:40 -0700

    :brain:

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ def dbQuery(query: str) -> None:
     """Query the Mysql database"""
     db = connect(host="localhost", user=argv[1],
                  passwd=argv[2], db=argv[3], port=3306)
-    cursor = db.cursor().
+    cursor = db.cursor()
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
