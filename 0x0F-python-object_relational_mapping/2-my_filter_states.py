51f0a1404d400cfccb808b5394c5adbe1da741b2
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-17 12:02:40 -0700

    :brain:

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a Mysqldb query function"""
+from MySQLdb import connect
+from sys import argv
+
+
+def dbQuery(query: str) -> None:
+    """Query the Mysql database"""
+    db = connect(host="localhost", user=argv[1],
+                 passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor().
+    cursor.execute(query.format(argv[4])
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'"
+    dbQuery(query)
