46cc919cf4e3c95b729b468d9bbfc2d15d33d8ea
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-17 12:07:32 -0700

    :brain:

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ def dbQuery(query: str) -> None:
     db = connect(host="localhost", user=argv[1],
                  passwd=argv[2], db=argv[3], port=3306)
     cursor = db.cursor().
-    cursor.execute(query.format()
+    cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
