8784442073712404dc3b5f2b63cc676ea37e57e1
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-03-18 02:19:33 -0700

    :zap:

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""Defines a Mysqldb query function"""
+from MySQLdb import connect
+from sys import argv
+
+
+def dbQuery(query: str) -> None:
+    """Query the Mysql database"""
+    db = connect(host="localhost", user=argv[1],
+                 passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
+    match = argv[4]
+    cursor.execute(query, (match, ))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    query = "SELECT * FROM states WHERE name LIKE %s"
+    dbQuery(query)
